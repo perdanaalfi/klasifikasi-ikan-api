85fef860ca14836a56b18b52a4178e86caacc846
perdanaalfi <perdanaalfi@gmail.com>
2025-07-15 21:56:01 +0700

    server.py

--- server.py
+++ server.py
@@ -50,7 +50,7 @@ def classify_fish(suhu, do, ph):
         except Exception as e:
             print("❌ Model error:", e)
     else:
-    print("Input tidak wajar")
+        print("Input tidak wajar")
 
     # Rule-based
     for ikan, batas in rules.items():
